a16fc4b56f1a0ac507c8988be904a400c5b4992a
Harshita Goel <167065803+Erharshita-cloud@users.noreply.github.com>
2026-01-08 19:59:01 +0530

    Create ec2.tf

---
+++ terraform/ec2.tf
@@ -0,0 +1,63 @@
+resource "aws_key_pair" "deployer" {
+  key_name   = "terra-automate-key"
+  public_key = file("${path.module}/terra-key.pub")
+}
+
+resource "aws_default_vpc" "default" {}
+
+resource "aws_security_group" "allow_user_to_connect" {
+  name        = "allow-ssh-http-https"
+  description = "Allow SSH, HTTP, HTTPS"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTPS"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "mysecurity"
+  }
+}
+
+resource "aws_instance" "testinstance" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.deployer.key_name
+  vpc_security_group_ids = [aws_security_group.allow_user_to_connect.id]
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "Automate"
+  }
+}
